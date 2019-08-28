2e069588078944c997d25014b28b88357216c7b0
sunwei <sunwei@aibeb.com>
2019-08-28 16:10:33 +0800

    add acorn

--- interpreter.js
+++ interpreter.js
@@ -1,36 +1,5 @@
-/**
- * @license
- * JavaScript Interpreter
- *
- * Copyright 2013 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+const acorn = require('acorn');
 
-/**
- * @fileoverview Interpreting JavaScript in JavaScript.
- * @author fraser@google.com (Neil Fraser)
- */
-'use strict';
-
-/**
- * Create a new interpreter.
- * @param {string|!Object} code Raw JavaScript text or AST.
- * @param {Function=} opt_initFunc Optional initialization function.  Used to
- *     define APIs.  When called it is passed the interpreter object and the
- *     global scope object.
- * @constructor
- */
 var Interpreter = function(code, opt_initFunc) {
   if (typeof code === 'string') {
     code = acorn.parse(code, Interpreter.PARSE_OPTIONS);
